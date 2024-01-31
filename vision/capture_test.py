edc9bd6c92ca73457274a012752ffe82c21ca076
ARNESH SAHA <arneshsaha@Arneshs-MacBook-Air-2.local>
2024-01-31 16:04:35 +0000

    Accepting centre coords

--- vision/capture_test.py
+++ vision/capture_test.py
@@ -67,6 +67,7 @@ while True:
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		print(center)
 		# only proceed if the radius meets a minimum size
 		if radius > 10:
 			# draw the circle and centroid on the frame,
