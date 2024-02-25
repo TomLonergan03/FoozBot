265ef037d397258cc50abdee7f32131afd2c5f2e
DougieIT <105588190+DougieIT@users.noreply.github.com>
2024-02-25 13:39:52 +0000

    Update PlayerController.py

--- PlayerController.py
+++ PlayerController.py
@@ -3,12 +3,10 @@ import time as time
 
 import ArduinoInterface
 
-serial_port = 'find correct port'
-baud_rate = 9600
 
 class PlayerController:
 
-    def __init__(self, width, ball_coords, first_row_y, second_row_y):
+    def __init__(self, bottom_right, ball_coords, first_row_y, second_row_y):
         self.ball_coords = ball_coords
 
         # These fields store the positions of the rows of players on the board
@@ -29,7 +27,7 @@ class PlayerController:
         self.first_row_horizontal = True
         self.second_row_horizontal = True
 
-        #Board width
+        # Board width
         self.width = width
 
     def update_ball_position(self, ball_coords):
@@ -55,28 +53,48 @@ class PlayerController:
 
     # Returns which player should hit the ball; 1, 2 or 3
     def which_players_zone(self, ball_coords):
-        if 0 < ball_coords < self.width/3:
+        if 0 < ball_coords < self.width / 3:
             return 1
-        if self.width/3 < ball_coords < 2*self.width/3:
+        if self.width / 3 < ball_coords < 2 * self.width / 3:
             return 2
-        if 2*self.width/3 < ball_coords < self.width:
+        if 2 * self.width / 3 < ball_coords < self.width:
             return 3
-
-        raise Exception("invalid ball position")
 
     def move_players(self, ball_coords):
         player_in_row = self.which_players_zone(ball_coords)
 
-        if (player_in_row == 1):
-            
+        # stepper motor moves from 0-110
+        # for each third we calculate the relative y position of the ball in that third (from 0 to 1)
+        # we then multiply this by the stepper
+        if player_in_row == 1:
+            top_of_zone = self.width / 3
+            bottom_of_zone = 0
+
+            motor_move_position = 110 * ((top_of_zone - ball_coords[1]) / (self.width / 3))
+            ArduinoInterface.ArduinoInterface.move_to(1, motor_move_position)
+            ArduinoInterface.ArduinoInterface.move_to(2, motor_move_position)
+
+        elif player_in_row == 2:
+            top_of_zone = 2 * self.width / 3
+            bottom_of_zone = self.width / 3
+            motor_move_position = 110 * (
+                        (top_of_zone - ball_coords[1]) / (top_of_zone - bottom_of_zone) + bottom_of_zone)
+            ArduinoInterface.ArduinoInterface.move_to(1, motor_move_position)
+            ArduinoInterface.ArduinoInterface.move_to(2, motor_move_position)
+        elif player_in_row == 3:
+            top_of_zone = self.width
+            bottom_of_zone = 2 * self.width / 3
+            motor_move_position = 110 * (
+                        (top_of_zone - ball_coords[1]) / (top_of_zone - bottom_of_zone) + bottom_of_zone)
+            ArduinoInterface.ArduinoInterface.move_to(1, motor_move_position)
+            ArduinoInterface.ArduinoInterface.move_to(2, motor_move_position)
 
     # Returns: (boolean, boolean) the bools represent if the ball is behind the first players and second players
     # respectively.
     def ball_behind_players(self):
-        return_val = (False, False)
+        return_val = [False, False]
         if self.ball_coords[1] < self.first_row_y:
             return_val[0] = True
-
 
         if self.ball_coords[1] < self.second_row_y:
             return_val[1] = True
@@ -86,27 +104,27 @@ class PlayerController:
     def first_row_go_horizontal(self):
         if not self.first_row_horizontal:
             self.first_row_horizontal = True
-            ArduinoInterface.go_horizontal(1)
+            ArduinoInterface.ArduinoInterface.go_horizontal(1)
 
     def first_row_go_vertical(self):
         if self.first_row_horizontal:
             self.first_row_horizontal = False
-            ArduinoInterface.go_horizontal(1)
+            ArduinoInterface.ArduinoInterface.go_horizontal(1)
 
     def second_row_go_horizontal(self):
         if not self.second_row_horizontal:
             self.second_row_horizontal = True
-            ArduinoInterface.go_horizontal(2)
+            ArduinoInterface.ArduinoInterface.go_horizontal(2)
 
     def second_row_go_vertical(self):
         if self.second_row_horizontal:
             self.second_row_horizontal = False
-            ArduinoInterface.go_vertical(2)
+            ArduinoInterface.ArduinoInterface(2)
 
     def first_row_kick(self):
         if not self.first_row_kicking:
             self.first_row_kicking = True
-            ArduinoInterface.kick(1)
+            ArduinoInterface.ArduinoInterface.kick(1)
 
     def second_row_kick(self):
         if not self.second_row_kicking:
