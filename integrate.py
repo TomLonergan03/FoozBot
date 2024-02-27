219bb11c01cc61b943ce0462f31471796dfeaa9b
ARNESH SAHA <arneshsaha@Arneshs-MacBook-Air-2.local>
2024-02-27 12:44:31 +0000

    updated integration

--- integrate.py
+++ integrate.py
@@ -1,7 +1,6 @@
 import serial
+from imutils.video import VideoStream
 import time
-import BallDetection as bd
-
 # Initialize serial connection (adjust '/dev/ttyACM0' as necessary)
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -9,9 +8,21 @@ def send_command(command):
     ser.write((command + "\n").encode())
     time.sleep(1)  # Ensure the Arduino has time to receive the command
 
-while True:
-    ball_position = bd.get_ball_center()
-    # Logic to determine ball position
-    if ball_position: # Let's have the ball's centre coords here
-        send_command("kick")
 
+while True:
+    print("""Potential Inputs: 
+    player stand
+    player anticlockwise
+    player clockwise
+    player horizontal
+    player kick
+    lateral reset
+    lateral in
+    lateral out
+    Press Q to exit!""")
+    cmd = input("Please enter your command: ")
+    if(cmd == 'q'):
+        print("Exit")
+        break
+    else:    
+        send_command(cmd)
