63d1352b9a95f16f10404b131c12e0bf57ebf7da
Douglas <douglastorrance@hotmail.com>
2024-03-08 18:07:00 +0000

    contrller display

--- Controller.py
+++ Controller.py
@@ -1,24 +1,33 @@
 import BallDetectionAdapter
-import TrajectoryAdapter
+from Mocks import MockArduinoInterface
 import PlayerController
-import field_bounds_detect
+import TrajectoryAdapter
 
 trajectory_finder = TrajectoryAdapter.TrajectoryAdapter(5, 10)
 
-# returns tuple (origin_x, origin_y, max_x, max_y)
-table_bounds = field_bounds_detect.get_table_bounds()
 
-ball_start_position = BallDetectionAdapter.get_ball_position()
+# arduino_interface = ArduinoInterface.ArduinoInterface()
+# This allows us to test the code when we don't have the arduino physically connected
+arduino_interface = MockArduinoInterface.MockArduinoInterface()
+
+# returns tuple (origin_x, origin_y, max_x, max_y)
+ball_vision = BallDetectionAdapter.BallEdgeDetection()
+bottom_right = ball_vision.get_bottom_right()
+
 
 FIRST_PLAYER_ROW = 5
 SECOND_PLAYER_ROW = 10
 
-player_controller = PlayerController.PlayerController(table_bounds, ball_start_position, FIRST_PLAYER_ROW, SECOND_PLAYER_ROW, table_bounds)
+#The table is measured from the top left (0,0), we need the bottom right (x,y) to define the playing field
+player_controller = PlayerController.PlayerController(ball_vision.get_ball_position(), FIRST_PLAYER_ROW, SECOND_PLAYER_ROW, bottom_right, arduino_interface)
 
 while True:
-    ball_position = BallDetectionAdapter.get_ball_position()
+    ball_position = ball_vision.get_ball_position(display=True)
+    bottom_right = ball_vision.get_bottom_right(display=True)
 
     new_player_row_intersections = trajectory_finder.get_new_intersections(ball_position)
 
     # This handles player movement
     player_controller.update_ball_position(new_player_row_intersections)
+
+    print("Ball position: " + str(ball_position) + "   " + str("Bottom right: " + str(bottom_right)))
