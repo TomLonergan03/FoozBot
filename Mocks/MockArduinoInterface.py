e51510c82d818ff3b9c9be288ed55f37bff55566
Douglas <douglastorrance@hotmail.com>
2024-03-08 15:16:10 +0000

    doug push

---
+++ Mocks/MockArduinoInterface.py
@@ -0,0 +1,26 @@
+class MockArduinoInterface():
+
+
+    def send_command(self, command):
+        return None
+
+    def go_vertical(self, player):
+        return None
+
+    def go_horizontal(self, player):
+        return None
+
+    def kick(self, player):
+        return None
+
+    # TODO
+    def move_to(player, position):
+        # need to implement
+        return None
+
+    # TODO
+    def get_position(self):
+        return None
+
+    def close(self):
+        return None
