010a3e746e78a24a4e168fcb20ac359a2004135a
Yichun Xiao <113861570+YichunX@users.noreply.github.com>
2024-03-25 21:29:17 +0000

    sensors.py

--- vision/sensors.py
+++ vision/sensors.py
@@ -4,9 +4,13 @@
 import serial
 import time
 
-# Arduino serial communication setup
-arduino_ser = serial.Serial('/dev/ttyACM0', 115200)  # Replace '/dev/ttyACM0' with the appropriate port
+# Arduino1 serial communication setup
+arduino_ser1 = serial.Serial('/dev/ttyACM0', 115200)  # Replace '/dev/ttyACM0' with the appropriate port
 time.sleep(2)  # Wait for the serial connection to be established
+
+# Arduino2 serial communication setup
+arduino_ser2 = serial.Serial('/dev/ttyACM1', 115200)
+time.sleep(2)
 
 # TF-Luna Mini LiDAR serial communication setup
 tfluna_ser = serial.Serial("/dev/ttyS0", 115200, timeout=0)
@@ -18,15 +22,25 @@ threshold = 50
 
 print('Starting Ranging...')
 while True:
-    # Read data from Arduino sensor
-    if arduino_ser.in_waiting > 0:
-        data = arduino_ser.readline().decode('utf-8').strip()
-        if data.startswith("Distance: "):
-            values = data.split("\t")
-            distance_str = values[0].split(": ")[1]
-            distance = float(distance_str[:-2])  # Remove the unit "cm" from the string
-            if distance <= threshold:
-                print(f"Spike detected on Arduino sensor: {distance} cm")
+    # Read data from Arduino sensor1
+    if arduino_ser1.in_waiting > 0:
+        data1 = arduino_ser1.readline().strip()
+        if data1.startswith(b"Distance: "):
+            values1 = data1.split(b"\t")
+            distance_str1 = values1[0].split(b": ")[1]
+            distance1 = float(distance_str1[:-2])  # Remove the unit "cm" from the string
+            if distance1 <= threshold:
+                print(f"Spike detected on Arduino sensor1: {distance1} cm")
+    
+    # read from arduino sensor 2
+    if arduino_ser2.in_waiting > 0:
+        data2 = arduino_ser2.readline().strip()
+        if data2.startswith(b"Distance: "):
+            values2 = data2.split(b"\t")
+            distance_str2 = values2[0].split(b": ")[1]
+            distance2 = float(distance_str2[:-2])  # Remove the unit "cm" from >
+            if distance2 <= threshold:
+                print(f"Spike detected on Arduino sensor2: {distance2} cm")
 
     # Read data from TF-Luna Mini LiDAR
     counter = tfluna_ser.in_waiting
