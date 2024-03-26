1307bea16e9b18d977bc642e5e04c90b43197ccc
DougieIT <105588190+DougieIT@users.noreply.github.com>
2024-03-26 14:12:25 +0000

    Create GoalSensorInterface.py
    Yichun's interface

---
+++ GoalSensorInterface.py
@@ -0,0 +1,10 @@
+class GoalSensorInterface:
+    def __init__(self):
+        self.player_1_score = 0
+        self.player_2_score = 0
+
+    def player_score(self, player_no):
+        if player_no == 1:
+            self.player_1_score += 1
+        if player_no ==2:
+            self.player_2_score += 1
