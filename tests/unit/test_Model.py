5888c751a0eb7282def3ffe248958791ee86af64
Arnesh <s2223898@ed.ac.uk>
2024-03-26 18:09:15 +0000

    Testing model attempt 2

--- tests/unit/test_Model.py
+++ tests/unit/test_Model.py
@@ -35,26 +35,6 @@ class TestModel(unittest.TestCase):
         self.assertAlmostEqual(future_location.x, 20, delta=0.1)
         self.assertAlmostEqual(future_location.y, 40, delta=0.1)
 
-class TestTrajectoryAdapter(unittest.TestCase):
-    def test_trajectory_adapter_initialization(self):
-        adapter = TrajectoryAdapter(player_row_1=100, player_row_2=200, top_left=(0, 0), bottom_right=(300, 300))
-        self.assertEqual(adapter.player_row_1, 100)
-        self.assertEqual(adapter.player_row_2, 200)
-        self.assertIsInstance(adapter.model, Model)
-
-    def test_get_new_intersections(self):
-        adapter = TrajectoryAdapter(player_row_1=100, player_row_2=200, top_left=(0, 0), bottom_right=(300, 300))
-        last_known_position = Location(50, 50, 0)
-        intersections = adapter.get_new_intersections(last_known_position)
-        self.assertIsInstance(intersections, list)
-        self.assertEqual(len(intersections), 2)
-
-    def test_draw_trajectory_on_frame(self):
-        adapter = TrajectoryAdapter(player_row_1=100, player_row_2=200, top_left=(0, 0), bottom_right=(300, 300))
-        image = cv2.imread("test_image.jpg")
-        trajectory = Trajectory([Location(0, 0, 0), Location(100, 100, 1), Location(200, 200, 2)])
-        adapter.draw_trajectory_on_frame(image, trajectory)
-        # Perform assertions to check if the trajectory is drawn correctly on the image
 
 if __name__ == '__main__':
     unittest.main()
